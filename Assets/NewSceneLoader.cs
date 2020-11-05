c41fc237dffdd12fa4de00408308bcf192caf4db
ara1098 <ara1098@gmail.com>
2020-11-05 18:48:46 -0500

    Main Menu Scene and ReBuild

---
+++ Assets/NewSceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewSceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadTerrain()
+    {
+        SceneManager.LoadScene("01-TerrainExperiment");
+
+    }
+    public void LoadSnaps()
+    {
+        SceneManager.LoadScene("02-Interiors");
+
+    }
+}
